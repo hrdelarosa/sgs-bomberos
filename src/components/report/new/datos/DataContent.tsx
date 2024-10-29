dc778319da1c94c7a648c940953097d0efd663ba
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2024-10-29 15:12:37 -0600

    Animation added to DataContent

--- src/components/report/new/datos/DataContent.tsx
+++ src/components/report/new/datos/DataContent.tsx
@@ -1,7 +1,19 @@
+import { useAutoAnimation } from '../../../../hooks/useAutoAnimation'
+
 interface Props {
   children: React.ReactNode
+  animation?: boolean
 }
 
-export default function DataContent({ children }: Props) {
-  return <div className="flex flex-row gap-3">{children}</div>
+// export default function DataContent({ children }: Props) {
+export default function DataContent({ children, animation = false }: Props) {
+  const { animationParent } = useAutoAnimation()
+  return (
+    <div
+      ref={animation ? animationParent : null}
+      className="flex flex-row gap-3"
+    >
+      {children}
+    </div>
+  )
 }
