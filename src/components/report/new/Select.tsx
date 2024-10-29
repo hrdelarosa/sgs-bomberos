0c6a32d1b3a4ef3f4d93199fef3f1dea2a928ce8
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2024-10-29 15:37:10 -0600

    New component for the datalist

---
+++ src/components/report/new/Select.tsx
@@ -0,0 +1,34 @@
+interface Props {
+  text: string
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  list: string[]
+}
+
+export default function Select({ text, handleChange, list }: Props) {
+  return (
+    <div className="flex flex-col mb-0.5">
+      <label
+        htmlFor="incidentes"
+        className="block text-gray-800 font-semibold text-sm"
+      >
+        {text}:
+      </label>
+      <div className="mt-2">
+        <input
+          className={`block text-sm h-[30px] rounded-md py-1 px-1.5 ring-1 ring-inset ring-gray-400 focus:text-gray-800`}
+          list={`list${text}`}
+          // id="incidentes"
+          name={`list${text}`}
+          placeholder={text}
+          onChange={handleChange}
+        />
+      </div>
+
+      <datalist id={`list${text}`}>
+        {list.map((incidente) => (
+          <option key={incidente} value={incidente}></option>
+        ))}
+      </datalist>
+    </div>
+  )
+}
