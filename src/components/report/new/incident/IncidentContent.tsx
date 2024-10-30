684ecad0382dce946b157b42baaacc136c2c48b2
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2024-10-30 14:01:43 -0600

    Added the remaining incidentType

--- src/components/report/new/incident/IncidentContent.tsx
+++ src/components/report/new/incident/IncidentContent.tsx
@@ -5,6 +5,8 @@ import {
   INCENDIO_INMUEBLES,
   INCENDIO_OTROS,
   FUGA_DERRAME,
+  RESCATE_EQUIPO,
+  ABEJAS,
 } from '../../../../constants/incidentList'
 import Input from '../datos/Input'
 
@@ -38,9 +40,44 @@ export default function IncidentContent({ incidentType }: Props) {
 
             <TextArea name="Especifique" type="EspecifiqueFugaDerrame" />
 
-            <Input name="Empresa" className="w-[170px]" />
+            <div className="flex flex-col justify-between">
+              <Input name="Empresa" className="w-[180px]" />
+              <Input name="No. de Guia" className="w-[180px]" />
+            </div>
 
-            {/* <div className="w-[1.5px] h-32 bg-gray-600 opacity-20"></div> */}
+            <div className="flex flex-col justify-between">
+              <Input name="Material" className="w-[200px]" />
+              <Input name="Observaciones" className="w-[200px]" />
+            </div>
+          </div>
+        </Card>
+      )}
+      {incidentType === 'Rescate' && (
+        <Card title="Rescate">
+          <div className="flex flex-row justify-center lg:justify-normal lg:gap-6 gap-5 p-4 lg:w-[870px]">
+            <div className="flex flex-col justify-between">
+              <Input name="Heridos" className="w-[170px]" />
+              <Input name="Muertos" className="w-[170px]" />
+            </div>
+
+            <TextArea name="Parte Ambulancia" type="RescateParteAmbulancia" />
+
+            <div className="w-[1.5px] h-32 mx-2 bg-gray-600 opacity-20"></div>
+
+            <div className="flex flex-col justify-between">
+              <Select text="Materiales" list={RESCATE_EQUIPO} />
+              <Input name="No. Personal" className="w-[170px]" />
+            </div>
+            <TextArea name="Especifique" type="EspecifiqueMateriales" />
+          </div>
+        </Card>
+      )}
+      {incidentType === 'Abejas' && (
+        <Card title="Abejas">
+          <div className="flex flex-row justify-center lg:justify-normal lg:gap-6 gap-5 p-4 lg:w-[870px]">
+            <Select text="Materiales" list={ABEJAS} />
+            <TextArea name="Especifique" type="EspecifiqueAbejas" />
+            <TextArea name="Observaciones" type="ObservacionesAbejas" />
           </div>
         </Card>
       )}
